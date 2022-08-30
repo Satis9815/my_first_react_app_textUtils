7f403bf8e78339de776f9cf981fffd3db63db253
Satis Kumar Chaudhary <66665573+Satis9815@users.noreply.github.com>
2022-08-30 06:29:45 +0545

    Add files via upload

---
+++ precache-manifest.cc3abb5fd86d80da8a47be1520be4073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc860f110bd2ef4cdbe4",
+    "url": "/static/css/main.57b0ddbf.chunk.css"
+  },
+  {
+    "revision": "bc860f110bd2ef4cdbe4",
+    "url": "/static/js/main.bc860f11.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "3976f5256a056c1de29f5e1ef9604e2d",
+    "url": "/index.html"
+  }
+];
